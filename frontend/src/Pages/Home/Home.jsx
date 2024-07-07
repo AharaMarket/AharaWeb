b80f02365ede4b7577a524ecaa38f2f0a42163ca
Nitish Padavala <padavala.nitish@gmail.com>
2024-07-07 14:06:47 -0700

    enabling landing

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
-// import Divider from '@mui/material/Divider';
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-// import AppAppBar from '../../Components/HomeComponents/AppAppBar';
-// import Hero from '../../Components/HomeComponents/Hero';
-// import LogoCollection from '../../Components/HomeComponents/LogoCollection';
-// import Highlights from '../../Components/HomeComponents/Highlights';
-// import Pricing from '../../Components/HomeComponents/Pricing';
-// import Features from '../../Components/HomeComponents/Features';
-// import Testimonials from '../../Components/HomeComponents/Testimonials';
-// import FAQ from '../../Components/HomeComponents/FAQ';
-// import Footer from '../../Components/HomeComponents/Footer';
+import AppAppBar from '../../Components/HomeComponents/AppAppBar';
+import Hero from '../../Components/HomeComponents/Hero';
+import LogoCollection from '../../Components/HomeComponents/LogoCollection';
+import Highlights from '../../Components/HomeComponents/Highlights';
+import Pricing from '../../Components/HomeComponents/Pricing';
+import Features from '../../Components/HomeComponents/Features';
+import Testimonials from '../../Components/HomeComponents/Testimonials';
+import FAQ from '../../Components/HomeComponents/FAQ';
+import Footer from '../../Components/HomeComponents/Footer';
 
 export default function LandingPage() {
   const [mode, setMode] = React.useState('light');
@@ -28,25 +28,26 @@ export default function LandingPage() {
   };
 
   return (
-    <h1></h1>
-    // <ThemeProvider theme={theme}>
-    //   <CssBaseline />
-    //   <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-    //   <Hero />
-    //   <Box sx={{ bgcolor: 'background.default' }}>
-    //     <LogoCollection />
-    //     <Features />
-    //     <Divider />
-    //     <Testimonials />
-    //     <Divider />
-    //     <Highlights />
-    //     <Divider />
-    //     <Pricing />
-    //     <Divider />
-    //     <FAQ />
-    //     <Divider />
-    //     <Footer />
-    //   </Box>
-    // </ThemeProvider>
+    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+      <Box sx={{ bgcolor: 'background.default' }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+    </div>
   );
 }
